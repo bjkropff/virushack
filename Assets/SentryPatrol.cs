bf2b333cfabadfc1447fb315c0edcb2375aff960
Tim Volpe <timothyvolpe@hotmail.com>
2015-04-18 12:40:08 -0700

    first version Sentry object

---
+++ Assets/SentryPatrol.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SentryPatrol : MonoBehaviour {
+
+	public GameObject target;
+	public float maxSpeed = 5.0f;
+	public float moveHorizontal = 1.0f;
+	float moveVertical = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (moveHorizontal * maxSpeed, moveVertical * maxSpeed);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		moveHorizontal = -moveHorizontal;
+	}
+
+
+}
