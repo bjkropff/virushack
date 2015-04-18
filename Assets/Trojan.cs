a0dce839ecdf7d6bcdb8ebe4d93dc816755ec67e
Tim Volpe <timothyvolpe@hotmail.com>
2015-04-18 15:14:06 -0700

    Trojan scripts: player, change pad, obstacles

---
+++ Assets/Trojan.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trojan : MonoBehaviour {
+
+	public Color currentColor = Color.white;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.name == "TrojanPad")
+		{
+			Color padColor = coll.gameObject.GetComponent<TrojanPad>().padColor;
+			currentColor = padColor;
+			this.gameObject.GetComponent<SpriteRenderer>().color = padColor;
+		}
+	}
+}
