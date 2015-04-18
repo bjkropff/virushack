e143800b6c171a3650f0b6ef077900becb9baa0c
Tim Volpe <timothyvolpe@hotmail.com>
2015-04-18 14:01:53 -0700

    Sentry patrol code updated, prefab made with stationary default

--- Assets/SentryPatrol.cs
+++ Assets/SentryPatrol.cs
@@ -4,23 +4,68 @@ using System.Collections;
 public class SentryPatrol : MonoBehaviour {
 
 	public GameObject target;
+	public string patrolType = "stationary";
 	public float maxSpeed = 5.0f;
-	public float moveHorizontal = 1.0f;
-	float moveVertical = 0.0f;
+	public float moveHorizontal = 0.0f;
+	public float moveVertical = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (moveHorizontal * maxSpeed, moveVertical * maxSpeed);
+		if (patrolType == "chase")
+			this.transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, maxSpeed * Time.deltaTime);
+		else
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (moveHorizontal * maxSpeed, moveVertical * maxSpeed);
 	}
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		moveHorizontal = -moveHorizontal;
+		switch (patrolType)
+		{
+			case "leftright":
+				moveHorizontal = -moveHorizontal;
+				break;
+
+			case "updown":
+				moveVertical = -moveVertical;
+				break;
+
+			case "clockwise":
+				if (moveHorizontal != 0)
+				{	
+					moveVertical = -moveHorizontal;
+					moveHorizontal = 0;
+				}
+				else
+				{
+					moveHorizontal = moveVertical;
+					moveVertical = 0;
+				}
+
+				break;
+		
+			case "counterclockwise":
+				if (moveHorizontal != 0)
+				{	
+					moveVertical = moveHorizontal;
+					moveHorizontal = 0;
+				}
+				else
+				{
+					moveHorizontal = -moveVertical;
+					moveVertical = 0;
+				}
+				break;
+
+			case "stationary":
+			default:
+				break;
+
+		}
 	}
 
 
