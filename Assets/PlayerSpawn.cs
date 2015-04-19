6a12922239820000fd74dadea46de356afe94b0c
Tom Mertz <tom.mertz@verizon.net>
2015-04-18 23:30:15 -0700

    Add player spawning at the PlayerSpawn prefab

--- Assets/PlayerSpawn.cs
+++ Assets/PlayerSpawn.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class PlayerSpawn : MonoBehaviour {
 
+	public GameObject player;
+
 	// Use this for initialization
 	void Start () {
-	
+		Instantiate(player, transform.position, Quaternion.identity);
 	}
 	
 	// Update is called once per frame
