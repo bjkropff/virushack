d1d8463a0ef3af3b7e93b1bb13dddd1e73d35500
Tim Volpe <timothyvolpe@hotmail.com>
2015-04-18 17:02:30 -0700

    wip on level select

---
+++ Assets/ExitLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.name == "Player")
+		{
+			Application.LoadLevel(0); // load level select	
+		}
+	}
+}
---
+++ Assets/SelectorScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectorScript : MonoBehaviour {
+
+	public void LoadScene(int level)
+	{
+		Application.LoadLevel(level);
+	}
+}
